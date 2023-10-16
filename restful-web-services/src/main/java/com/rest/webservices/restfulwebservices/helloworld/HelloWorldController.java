8b65435344fc03467ff77bf6f9b85a2ddadfbcc9
Lilall5829 <lilall5829@gmail.com>
2023-10-15 22:55:25 -0400

    1015

--- restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,12 +1,22 @@
 package com.rest.webservices.restfulwebservices.helloworld;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 //REST API
 @RestController
 public class HelloWorldController {
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
     //hello-world
 //    @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
 //    public String helloworld(){
@@ -29,7 +39,10 @@ public class HelloWorldController {
 // Internationalized
     @GetMapping(path = "/hello-world-internationalized")
     public String helloworldInternationalized(){
-        return "Hello World";
+        //Get the locale
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null,"Default Message", locale);
+//        return "Hello World";
     }
 
 }
