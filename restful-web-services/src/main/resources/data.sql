9038f94fff08510586cd9bd38460aa1ec1bf02fd
Lilall5829 <lilall5829@gmail.com>
2023-10-18 19:25:36 -0400

    1018

---
+++ restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+--  Column name should be consistent with H2 database
+insert into user_details(id, birth_Date, name)
+values(10001, current_date(), 'lila1');
+
+insert into user_details(id, birth_Date, name)
+values(10002, current_date(), 'lila2');
+
+insert into user_details(id, birth_Date, name)
+values(10003, current_date(), 'lila3');
