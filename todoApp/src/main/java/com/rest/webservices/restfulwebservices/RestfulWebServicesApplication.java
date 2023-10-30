b809b4cb01c062005b39e05131072bf28ed9a90e
Lilall5829 <lilall5829@gmail.com>
2023-10-30 13:50:04 -0400

    1030 Back end of todo app

--- todoApp/src/main/java/com/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ todoApp/src/main/java/com/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -2,12 +2,30 @@ package com.rest.webservices.restfulwebservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RestfulWebServicesApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RestfulWebServicesApplication.class, args);
+    }
+
+    // Handle CORS: Allow all request only from localhost:3000 to 8080
+    // Search method addCorsMappings from interface WebMvcConfigurer
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+
+        return new WebMvcConfigurer() {
+            // Override addCorsMappings
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                // Allow any URl and any request from port 3000
+                registry.addMapping("/**").allowedMethods("*").allowedOrigins("http://localhost:3000");
+            }
+        };
     }
 
 }
